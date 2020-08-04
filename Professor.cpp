c41791e4a22a8f0d3552eba1a705e7a749188f39
rafael cerdas <rafacerdas9@gmail.com>
2020-08-04 12:18:56 -0600

    para prueba

--- Professor.cpp
+++ Professor.cpp
@@ -5,3 +5,34 @@
 #include <stdexcept>
 #include <sstream>
 #include "Professor.h"
+
+
+Professor::Professor(const firstName &name, double monthlySalary, double commissionRate) : Person(name),
+                                                                                           monthlySalary(monthlySalary),
+                                                                                           commissionRate(
+                                                                                                   commissionRate) {}
+
+void Professor::setMonthlySalary(double monthlySalary) {
+    Professor::monthlySalary = monthlySalary;
+}
+
+void Professor::setCommissionRate(double commissionRate) {
+    Professor::commissionRate = commissionRate;
+}
+
+double Professor::getMonthlySalary() const {
+    return monthlySalary;
+}
+
+double Professor::getCommissionRate() const {
+    return commissionRate;
+}
+
+Professor::~Professor() {
+
+}
+
+const double Professor::salary() {
+    return Person::salary();
+}
+
