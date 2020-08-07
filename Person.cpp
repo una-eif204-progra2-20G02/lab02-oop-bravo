beb0b27f055acf16f3f662ba162df26f5e6b766a
njesquivel <njesquivel@gmail.com>
2020-08-07 00:48:53 -0600

    Metodos basicos implementados

--- Person.cpp
+++ Person.cpp
@@ -4,4 +4,39 @@
 
 #include "Person.h"
 
-Person::Person(const string &firstName, const string &lastName, int documentId) : firstName(firstName),lastName(lastName),documentId(documentId) {}
+Person::Person() {
+
+}
+
+Person::Person( string &firstName, string &lastName, int documentId) : firstName(firstName), lastName(lastName), documentId(documentId) {}
+
+Person::~Person() {
+
+}
+
+
+const string &Person::getFirstName() const {
+    return firstName;
+}
+
+void Person::setFirstName(const string &firstName) {
+    Person::firstName = firstName;
+}
+
+const string &Person::getLastName() const {
+    return lastName;
+}
+
+void Person::setLastName(const string &lastName) {
+    Person::lastName = lastName;
+}
+
+int Person::getDocumentId() const {
+    return documentId;
+}
+
+void Person::setDocumentId(int documentId) {
+    Person::documentId = documentId;
+}
+
+
--- University.cpp
+++ University.cpp
@@ -4,3 +4,59 @@
 
 #include <sstream>
 #include "University.h"
+
+University::University(const string &name, const Professor &professor, const Administrative &administrative,
+                       const vector<Professor *> &professorList, const vector<Administrative *> &administrativeList)
+        : name(name), professor(professor), administrative(administrative), professorList(professorList),
+          administrativeList(administrativeList) {}
+
+const string &University::getName() const {
+    return name;
+}
+
+void University::setName(const string &name) {
+    University::name = name;
+}
+
+const Professor &University::getProfessor() const {
+    return professor;
+}
+
+void University::setProfessor(const Professor &professor) {
+    University::professor = professor;
+}
+
+const Administrative &University::getAdministrative() const {
+    return administrative;
+}
+
+void University::setAdministrative(const Administrative &administrative) {
+    University::administrative = administrative;
+}
+
+const vector<Professor *> &University::getProfessorList() const {
+    return professorList;
+}
+
+void University::setProfessorList(const vector<Professor *> &professorList) {
+    University::professorList = professorList;
+}
+
+const vector<Administrative *> &University::getAdministrativeList() const {
+    return administrativeList;
+}
+
+void University::setAdministrativeList(const vector<Administrative *> &administrativeList) {
+    University::administrativeList = administrativeList;
+}
+
+
+
+University::~University() {
+
+}
+
+
+
+
+
